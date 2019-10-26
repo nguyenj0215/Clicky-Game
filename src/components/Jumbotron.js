cb522af3fad1561eb5f0e3facd9bfd46f4283fe9
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-10-25 22:09:56 -0700

    navbar and jumbrotron added

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Jumbotron} from 'reactstrap';
+
+const JumbotronBox = (props) => {
+    return (
+        <Jumbotron>
+            <h1 className="display-3">Clicky Game</h1>
+            <p className="lead">Click on an image to earn points, but don't click on any more than once!</p>
+            <hr className="my-2" />
+            <p>Display for score and top score</p>
+        </Jumbotron>
+
+    );
+};
+
+export default JumbotronBox;
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
+
+const NavTab = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">Clicky Game</NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <NavLink href="https://github.com/nguyenj0215/clicky-game" target="_blank">GitHub</NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+        </Navbar>
+    );
+}
+
+export default NavTab;
