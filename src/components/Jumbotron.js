dab506e71e349d4f5cc1fdedb41ad50388061019
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-10-27 21:31:11 -0700

    heroku check

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -4,7 +4,7 @@ import { Jumbotron} from 'reactstrap';
 const JumbotronBox = (props) => {
     return (
         <Jumbotron className="jumbotron">
-            <h1 className="display-3 jumbotronTitle">Clicky Game - NFL Edition</h1>
+            <h1 className="display-3 jumbotronTitle">Clicky Game - NFL Edition!</h1>
             <p className="lead jumbotronDescription">Click on an image to earn points but don't click on any more than once!</p>
             <hr className="my-2" />
             <p className="highDisplay">High score: {props.high}</p>
