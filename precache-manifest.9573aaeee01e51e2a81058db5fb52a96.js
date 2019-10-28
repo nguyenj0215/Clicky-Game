a42dff7c09a09ce7dc443dfb0fd381d476c7503d
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-10-27 21:40:36 -0700

    Updates

---
+++ precache-manifest.9573aaeee01e51e2a81058db5fb52a96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88facfae110c3d707aeb5b736cd8158e",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b4532166b658c285deb1",
+    "url": "/clicky-game/static/css/main.8fbf6de0.chunk.css"
+  },
+  {
+    "revision": "ad61de288c66b5907d35",
+    "url": "/clicky-game/static/js/2.45b924ff.chunk.js"
+  },
+  {
+    "revision": "b4532166b658c285deb1",
+    "url": "/clicky-game/static/js/main.563dd7ef.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
